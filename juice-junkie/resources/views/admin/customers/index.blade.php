f7f6f387dbdab5c0656b1bd4be605e66c49f3dd9
WincentCP <wincentcoleusphan@gmail.com>
2025-05-05 10:23:12 +0700

    Update index.blade.php

--- juice-junkie/resources/views/admin/customers/index.blade.php
+++ juice-junkie/resources/views/admin/customers/index.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.admin')
 
-@section('title', 'Daftar Pelanggan')
-
 @section('content')
     <div class="flex justify-between items-center mb-6">
         <h1 class="text-2xl font-semibold">Daftar Pelanggan</h1>
